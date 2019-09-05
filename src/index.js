e296a6969564939a5e65f7252ab552a4df3947ae
Dinesh123 <dinesh.kumar@wildjasmine.com>
2019-09-05 18:43:47 +0530

    proj setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// const App = () => {
+//   return (
+//     <div>
+//       <p>React here!</p>
+//     </div>
+//   );
+// };
+// export default App;
+ReactDOM.render(console.log("hiiiiiii"), document.getElementById("app"));
